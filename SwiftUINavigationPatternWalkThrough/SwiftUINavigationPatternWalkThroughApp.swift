bb3b67d6cd1a3a00628958f210288629be14d1f1
yongjuncha <perfect_yon@naver.com>
2021-12-30 13:48:06 +0900

    Initial Commit

---
+++ SwiftUINavigationPatternWalkThrough/SwiftUINavigationPatternWalkThroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationPatternWalkThroughApp.swift
+//  SwiftUINavigationPatternWalkThrough
+//
+//  Created by Yong Jun Cha on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationPatternWalkThroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
